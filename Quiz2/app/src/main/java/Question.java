41b0d897ef5099d56ee192f2498b25a7931f64d4
izaostojska <147402977+izaostojska@users.noreply.github.com>
2023-10-09 15:27:58 +0200

    Add files via upload

---
+++ Quiz2/app/src/main/java/Question.java
@@ -0,0 +1,9 @@
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer){
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+}
